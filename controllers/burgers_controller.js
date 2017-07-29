a5a1547d72529daaebf34dee48e9dd04ca91adf2
MDC_Desktop <marya.crigler@att.net>
2017-07-29 17:46:54 -0500

    update homework files 20170729

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,64 @@
+// Required depedencies
+var express = require('express');
+var router = express.Router();
+var burger = require('../models/burger.js');
+
+// Create routes
+
+// Index redirect
+router.get('/', function(req, res) {
+	res.redirect('/index');
+});
+
+
+// selectAll
+router.get('/index', function(req, res) {
+	burger.selectAll(function(data) {
+		var hbsObject = {burgers: data};
+		res.render('index', hbsObject);
+	});
+});
+
+
+
+// insertOne
+router.post('/burgers/insertOne', function(req, res) {
+	burger.insertOne(['burger_name', 'devoured'], [req.body.name, false], function() {
+		res.redirect('/index');
+	});
+});
+
+
+
+// updateOne
+router.put('/burgers/updateOne/:id', function(req, res) {
+	var condition = 'id = ' + req.params.id;
+	console.log('condition', condition);
+
+	burger.updateOne({devoured: req.body.devoured}, condition, function() {
+		res.redirect('/index');
+	});
+});
+
+
+/*
+// Create a New Burger
+router.post('/burger/create', function (req, res) {
+  burger.insertOne(req.body.burger_name, function() {
+    res.redirect('/index');
+  });
+});
+
+
+// Devour a Burger
+router.post('/burger/eat/:id', function (req, res) {
+  burger.updateOne(req.params.id, function() {
+    res.redirect('/index');
+  });
+});
+// ----------------------------------------------------
+
+*/
+
+// Export 
+module.exports = router;
